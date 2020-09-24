39e0ba17a7b8abc1672a82eecfe7408f3869ac0c
53jk1 <bonczalke@gmail.com>
2020-09-25 00:08:24 +0200

    modified:   src/main/java/com/example/projectjh/converters/GameCommandToGame.java modified:   src/main/java/com/example/projectjh/model/Game.java

--- src/main/java/com/example/projectjh/converters/GameCommandToGame.java
+++ src/main/java/com/example/projectjh/converters/GameCommandToGame.java
@@ -2,6 +2,8 @@ package com.example.projectjh.converters;
 
 import com.example.projectjh.commands.GameCommand;
 import com.example.projectjh.model.Game;
+import com.example.projectjh.repositories.DeveloperRepository;
+import com.example.projectjh.repositories.PublisherRepository;
 import org.springframework.lang.Nullable;
 import lombok.Synchronized;
 import org.springframework.core.convert.converter.Converter;
@@ -9,6 +11,15 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class GameCommandToGame implements Converter<GameCommand, Game> {
+
+    private DeveloperRepository developerRepository;
+    private PublisherRepository publisherRepository;
+
+    public GameCommandToGame(DeveloperRepository developerRepository, PublisherRepository publisherRepository) {
+        this.developerRepository = developerRepository;
+        this.publisherRepository = publisherRepository;
+    }
+
     @Synchronized
     @Nullable
     @Override
@@ -16,11 +27,14 @@ public class GameCommandToGame implements Converter<GameCommand, Game> {
         if (source == null) {
             return null;
         }
+
+        /**
         final Game game = new Game();
         game.setTitle(source.getTitle());
         game.setGenre(source.getGenre());
         game.setYear(source.getYear());
 
         return game;
+         */
     }
 }
--- src/main/java/com/example/projectjh/model/Game.java
+++ src/main/java/com/example/projectjh/model/Game.java
@@ -1,16 +1,20 @@
 package com.example.projectjh.model;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
+
 @Data
+@NoArgsConstructor
+@EqualsAndHashCode(of = {"id"})
 @Entity
 public class Game {
     @Id
-    @GeneratedValue
-
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String title;
     private String genre;
@@ -18,7 +22,9 @@ public class Game {
 
     @ManyToOne
     private Publisher publisher;
-    private Set<Developer> developer = new HashSet<>();
+
+    @ManyToMany
+    private Set<Developer> developers = new HashSet<>();
 
     public Game(String title, String genre, String year, Publisher publisher) {
         this.title = title;
@@ -27,66 +33,9 @@ public class Game {
         this.publisher = publisher;
     }
 
-    public Game() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
+    public Game(String title, String genre, String year) {
         this.title = title;
-    }
-
-    public String getGenre() {
-        return genre;
-    }
-
-    public void setGenre(String genre) {
         this.genre = genre;
-    }
-
-    public String getYear() {
-        return year;
-    }
-
-    public void setYear(String year) {
         this.year = year;
-    }
-
-    public Publisher getPublisher() {
-        return publisher;
-    }
-
-    public void setPublisher(Publisher publisher) {
-        this.publisher = publisher;
-    }
-
-    public Set<Developer> getDeveloper() {
-        return developer;
-    }
-
-    public void setDeveloper(Set<Developer> developer) {
-        this.developer = developer;
-    }
-
-    @Override
-    public String toString() {
-        return "Game{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", genre='" + genre + '\'' +
-                ", year='" + year + '\'' +
-                ", publisher='" + publisher + '\'' +
-                ", developers=" + developer +
-                '}';
     }
 }
