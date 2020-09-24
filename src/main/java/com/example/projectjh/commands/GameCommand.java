13f4dd4534acfbb0cb24b0385ed8af768f5df07d
53jk1 <bonczalke@gmail.com>
2020-09-25 00:10:51 +0200

    modified:   src/main/java/com/example/projectjh/commands/GameCommand.java

--- src/main/java/com/example/projectjh/commands/GameCommand.java
+++ src/main/java/com/example/projectjh/commands/GameCommand.java
@@ -9,39 +9,7 @@ import lombok.Setter;
 @NoArgsConstructor
 public class GameCommand {
     private Long id;
-    private String title ;
-    private String genre ;
-    private String year ;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getGenre() {
-        return genre;
-    }
-
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
-
-    public String getYear() {
-        return year;
-    }
-
-    public void setYear(String year) {
-        this.year = year;
-    }
+    private String title;
+    private String genre;
+    private String year;
 }
