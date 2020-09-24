feccdbf53ef94a3dca1d1596426eb3707b19c58a
53jk1 <bonczalke@gmail.com>
2020-09-24 23:53:17 +0200

    modified:   src/main/java/com/example/projectjh/repositories/GameRepository.java

--- src/main/java/com/example/projectjh/repositories/GameRepository.java
+++ src/main/java/com/example/projectjh/repositories/GameRepository.java
@@ -1,10 +1,15 @@
 package com.example.projectjh.repositories;
 
+import com.example.projectjh.model.Developer;
 import com.example.projectjh.model.Game;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
 
 
 public interface GameRepository extends CrudRepository <Game,Long>{
 
+    List<Game> getAllByDevelopersIsContaining(Developer developer);
+    Optional<Game> getFirstByHq(String hq); // You can set whatever you want here.
 }
