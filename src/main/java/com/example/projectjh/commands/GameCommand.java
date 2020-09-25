ed23ecfa36668546d1f7aae952e8588b3bcb826d
53jk1 <bonczalke@gmail.com>
2020-09-26 00:25:14 +0200

    modified:   src/main/java/com/example/projectjh/commands/GameCommand.java

--- src/main/java/com/example/projectjh/commands/GameCommand.java
+++ src/main/java/com/example/projectjh/commands/GameCommand.java
@@ -1,5 +1,7 @@
 package com.example.projectjh.commands;
 
+import com.example.projectjh.model.Developer;
+import com.example.projectjh.model.Publisher;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -12,4 +14,6 @@ public class GameCommand {
     private String title;
     private String genre;
     private String year;
+    private Long developerId;
+    private Long publisherId;
 }
