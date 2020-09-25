8587ef4246fc604bbd255be2e32e77efb5f3af21
53jk1 <bonczalke@gmail.com>
2020-09-26 00:28:47 +0200

    modified:   src/main/java/com/example/projectjh/commands/PublisherCommand.java

--- src/main/java/com/example/projectjh/commands/PublisherCommand.java
+++ src/main/java/com/example/projectjh/commands/PublisherCommand.java
@@ -1,4 +1,13 @@
 package com.example.projectjh.commands;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class PublisherCommand {
+    private Long id;
+    private String name;
 }
