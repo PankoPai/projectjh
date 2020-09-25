3406f5eed79d198a952621f64b0cba6a1344da5f
53jk1 <bonczalke@gmail.com>
2020-09-26 00:50:39 +0200

    modified:   src/main/java/com/example/projectjh/converters/DeveloperCommandToDeveloper.java

--- src/main/java/com/example/projectjh/converters/DeveloperCommandToDeveloper.java
+++ src/main/java/com/example/projectjh/converters/DeveloperCommandToDeveloper.java
@@ -1,11 +1,15 @@
 package com.example.projectjh.converters;
 
 import com.example.projectjh.commands.DeveloperCommand;
-import com.example.projectjh.model.Developer;
+import com.example.projectjh.commands.GameCommand;
+import com.example.projectjh.model.*;
+import com.example.projectjh.repositories.DeveloperRepository;
+import com.example.projectjh.repositories.PublisherRepository;
 import lombok.Synchronized;
-import org.springframework.core.convert.converter.Converter;
-import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+import org.springframework.lang.Nullable;
 
 @Component
 public class DeveloperCommandToDeveloper implements Converter<DeveloperCommand, Developer> {
