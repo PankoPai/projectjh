f425b39c6b67c32549d3d6f2e8643b75d1b7d9aa
53jk1 <bonczalke@gmail.com>
2020-09-26 01:03:30 +0200

    hello new problems with database

--- src/main/java/com/example/projectjh/model/Developer.java
+++ src/main/java/com/example/projectjh/model/Developer.java
@@ -1,1 +1,32 @@
-xxx
+package com.example.projectjh.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@EqualsAndHashCode(of = {"id"})
+@Entity
+public class Developer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String year;
+    private String country;
+    private String hq;
+
+    @ManyToMany(mappedBy = "developers")
+    private Set<Game> games = new HashSet<>();
+
+    public Developer() {
+    }
+    public Developer(String name, String year, String country, String hq) {
+        this.name = name;
+        this.year = year;
+        this.country = country;
+        this.hq = hq;
+    }
+}
